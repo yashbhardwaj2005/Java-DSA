dd9cce1982882f9834d46d4427fe051bdf86d267
yashbhardwaj001 <yashbhardwaj509@gmail.com>
2026-08-05 15:54:34 +0530

    push this question in main

---
+++ src/arrays/Leetcode_1920.java
@@ -0,0 +1,36 @@
+/*
+Problem: 1920. Build Array from Permutation
+
+Approach:
+1. Create answer array.
+2. Traverse the array.
+3. ans[i] = nums[nums[i]]
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+public class Leetcode_1920 {
+
+    public static int[] buildArray(int[] nums) {
+
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {0, 2, 1, 5, 3, 4};
+
+        int[] ans = buildArray(nums);
+
+        for (int x : ans) {
+            System.out.print(x + " ");
+        }
+    }
+}
