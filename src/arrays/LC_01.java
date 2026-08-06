af8fabeddcc7ae353193c3d623d63141df60425c
yashbhardwaj001 <yashbhardwaj509@gmail.com>
2026-08-07 00:43:15 +0530

    push this leetcode question

--- src/arrays/LC_01.java
+++ src/arrays/LC_01.java
@@ -1,7 +1,7 @@
 //#01)    You are given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 //        You may assume that each input would have exactly one solution, and you may not use the same element twice.
 //
-//        You can return the answer in any order.
+//        You can return the answer in any order
 //
 //       TWO SUM :-
 
