cf0036fa62da4646e5838198a5ca0148520ec6b8
yashbhardwaj001 <yashbhardwaj509@gmail.com>
2026-08-08 15:29:55 +0530

    Add String linear search

---
+++ src/LinearSearch/LS_02.java
@@ -0,0 +1,43 @@
+//SEARCH IN STRINGS :-
+//
+// Here str.length() is used as it a method/function and arr.length is a field/property
+
+package LinearSearch;
+import java.util.Arrays;
+
+public class LS_02 {
+    public static void main(String[] args) {
+        String name = "Kunal";
+        char target = 'u';
+//        System.out.println(search(name, target));
+
+        System.out.println(Arrays.toString(name.toCharArray()));
+    }
+
+
+    static boolean search2(String str, char target) {
+        if (str.length() == 0) {
+            return false;
+        }
+
+        for(char ch : str.toCharArray()) {
+            if (ch == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean search(String str, char target) {
+        if (str.length() == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (target == str.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
