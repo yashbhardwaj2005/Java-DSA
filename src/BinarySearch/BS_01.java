fdeccf485574d65beb02cfe03a0dd70ed802bea7
yashbhardwaj001 <yashbhardwaj509@gmail.com>
2026-08-10 16:57:37 +0530

    add proper comments

--- src/BinarySearch/BS_01.java
+++ src/BinarySearch/BS_01.java
@@ -15,7 +15,7 @@ public class BS_01 {
         int end = arr.length - 1;
 
         while (start <= end) {
-
+//          find the middle element:-
 //          int mid = start + end / 2;      // might be possible that start + end exceeds the range of int in java
             int mid = start + (end - start) / 2;
 
