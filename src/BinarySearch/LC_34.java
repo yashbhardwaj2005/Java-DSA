1a5a1904e43228fbba9e910bbd7725b5d9cac3fb
yashbhardwaj001 <yashbhardwaj509@gmail.com>
2026-08-12 23:57:12 +0530

    Add comments related to questions in LC_34

--- src/BinarySearch/LC_34.java
+++ src/BinarySearch/LC_34.java
@@ -2,7 +2,7 @@
 
 //Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
 //If target is not found in the array, return [-1, -1].
-//You must write an algorithm with O(log n) runtime complexity.
+//You must write an algorithm with O(log n) runtime complexity .
 
 package BinarySearch;
 
