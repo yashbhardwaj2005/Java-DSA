aa2555f46e8d1c8ed9797908f6ec05585a2804c5
yashbhardwaj001 <yashbhardwaj509@gmail.com>
2026-08-12 23:56:17 +0530

    Add comments related to questions in LC_34

--- src/BinarySearch/LC_34.java
+++ src/BinarySearch/LC_34.java
@@ -1,4 +1,4 @@
-// Find First and Last Position of Element in Sorted Array :-
+// #34) Find First and Last Position of Element in Sorted Array :-
 
 //Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
 //If target is not found in the array, return [-1, -1].
