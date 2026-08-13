6d99547225044c766706e702b05ab334756b5e3d
yashbhardwaj001 <yashbhardwaj509@gmail.com>
2026-08-13 23:55:28 +0530

    add comments in  basic binary search question

--- src/BinarySearch/LC_704.java
+++ src/BinarySearch/LC_704.java
@@ -1,7 +1,9 @@
+// #704 )BINARY SEARCH BASIC QUESTION :-
 
+//Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. \
+//If target exists, then return its index. Otherwise, return -1.
 
-
-
+//You must write an algorithm with O(log n) runtime complexity.
 
 package BinarySearch;
 
