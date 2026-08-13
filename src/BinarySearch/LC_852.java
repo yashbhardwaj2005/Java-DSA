c68a92ae304ddb8b6516fec0c7ae2dee72691d8b
yashbhardwaj001 <yashbhardwaj509@gmail.com>
2026-08-13 23:59:34 +0530

    update comments in finding peak index in mountain array question

--- src/BinarySearch/LC_852.java
+++ src/BinarySearch/LC_852.java
@@ -1,3 +1,11 @@
+// #852 ) Peak Index in a Mountain Array :-
+
+// You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+
+//Return the index of the peak element.
+
+//Your task is to solve it in O(log(n)) time complexity.
+
 package BinarySearch;
 
 public class LC_852 {
